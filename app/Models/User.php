acd0f9553ca62961bcf35c08fd4dcda283ec5d83
AMK1215 <aungmyokhaing1215@gmail.com>
2024-09-28 09:43:28 +0630

    git

--- app/Models/User.php
+++ app/Models/User.php
@@ -135,7 +135,7 @@ class User extends Authenticatable implements Wallet
 
     public static function adminUser()
     {
-        return self::where('type', UserType::Player)->first();
+        return self::where('type', UserType::Admin)->first();
     }
 
     public function seamlessTransactions()
