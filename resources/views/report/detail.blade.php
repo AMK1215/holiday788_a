23f7002ac510265d396df91fe8fa739b5691b781
AMK1215 <aungmyokhaing1215@gmail.com>
2024-09-28 15:44:31 +0630

    bootstrap

--- resources/views/report/detail.blade.php
+++ resources/views/report/detail.blade.php
@@ -10,7 +10,7 @@
     <script src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
     {{-- <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> --}}
-    <link rel="stylesheet" href="{{ asset('user_app/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('user_app/css/bootstrap.css') }}">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css">
 </head>
 
