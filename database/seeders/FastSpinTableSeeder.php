96f32675f7d638542d5293df1ca8e0a2237690bf
AMK1215 <aungmyokhaing1215@gmail.com>
2024-10-11 10:52:21 +0630

    game updated by amk

--- database/seeders/FastSpinTableSeeder.php
+++ database/seeders/FastSpinTableSeeder.php
@@ -2,20 +2,19 @@
 
 namespace Database\Seeders;
 
-use App\Models\Admin\GameList;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\File;
+use App\Models\Admin\GameList;
 
-
-class JiliTablesSeeder extends Seeder
+class FastSpinTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $json = File::get(base_path('app/Console/Commands/data/JiliNewModify.json'));
+        $json = File::get(base_path('app/Console/Commands/data/FastSpinModify.json'));
         $data = json_decode($json);
         foreach ($data->ProviderGames as $obj) {
             GameList::create([
